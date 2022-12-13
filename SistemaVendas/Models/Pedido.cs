febe7aa7e98169f99e58d231c4f06c309f2254e6
Giovanni Benner <giovanni.guilioti@benner.com.br>
2022-12-13 10:47:43 -0300

    models

---
+++ SistemaVendas/Models/Pedido.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public int VendedorId { get; set; }
+        public Vendedor Vendedor { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+
+        //public decimal ValorTotal { get; set; }
+    }
+}
