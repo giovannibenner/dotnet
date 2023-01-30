cded48800b459ddbbe3b61bec817b7fce0a17ac3
Giovanni Benner <giovanni.guilioti@benner.com.br>
2023-01-30 14:41:39 -0300

    ServicoDTO Id included

--- SistemaVendas/Dto/ObterServicoDTO.cs
+++ SistemaVendas/Dto/ObterServicoDTO.cs
@@ -4,11 +4,13 @@ namespace SistemaVendas.Dto
 {
     public class ObterServicoDTO
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
 
         public ObterServicoDTO(Servico servico)
         {
+            Id = servico.Id;
             Nome = servico.Nome;
             Descricao = servico.Descricao;
         }
