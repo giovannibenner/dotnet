29ead7e9238dafc6f79e22b4e12fbe58e62fb229
Giovanni Benner <giovanni.guilioti@benner.com.br>
2023-01-30 13:57:17 -0300

    PedidoDTO Id included

--- SistemaVendas/Dto/ObterPedidoDTO.cs
+++ SistemaVendas/Dto/ObterPedidoDTO.cs
@@ -4,12 +4,14 @@ namespace SistemaVendas.Dto
 {
     public class ObterPedidoDTO
     {
+        public int Id { get; set; }
         public DateTime Data { get; set; }
         public int VendedorId { get; set; }
         public int ClienteId { get; set; }
 
         public ObterPedidoDTO(Pedido pedido)
         {
+            Id = pedido.Id;
             Data = pedido.Data;
             VendedorId = pedido.VendedorId;
             ClienteId = pedido.ClienteId;
