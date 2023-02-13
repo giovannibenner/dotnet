c9d3c5033e8aee8b49df450f8ca11da394abd62d
Giovanni Benner <giovanni.guilioti@benner.com.br>
2023-02-13 08:54:50 -0300

    Add Senha to ObterClienteDTO

--- SistemaVendas/Dto/ObterClienteDTO.cs
+++ SistemaVendas/Dto/ObterClienteDTO.cs
@@ -11,12 +11,14 @@ namespace SistemaVendas.Dto
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Login { get; set; }
+        public string Senha { get; set; }
 
         public ObterClienteDTO(Cliente cliente)
         {
             Id = cliente.Id;
             Nome = cliente.Nome;
             Login = cliente.Login;
+            Senha = cliente.Senha;
         }
     }
 }
